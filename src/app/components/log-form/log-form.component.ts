47ee1c03905d17acd35195283ed1ade168a38612
Rafal Kelm <rafal.kelm@gmail.com>
2019-08-17 21:14:45 +0200

    DevLogger deployment

---
+++ src/app/components/log-form/log-form.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { LogService } from '../../services/log.service';
+import { Log } from '../../models/Log';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-log-form',
+  templateUrl: './log-form.component.html',
+  styleUrls: ['./log-form.component.css']
+})
+export class LogFormComponent implements OnInit {
+  id: string;
+  text: string;
+  date: any;
+
+  isNew: boolean = true;
+
+  constructor(private logService: LogService) { }
+
+  ngOnInit() {
+    // Subscribe to the selectedLog observable
+    this.logService.selectedLog.subscribe(log => {
+      if (log.id !== null) {
+        this.isNew = false;
+        this.id = log.id;
+        this.text = log.text;
+        this.date = log.date;
+      }
+    });
+  }
+
+  onSubmit() {
+    // Check if new log
+    if (this.isNew) {
+      // create a new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+    };
+      // Add log
+      console.log(newLog.id);
+      this.logService.addLog(newLog);
+    } else {
+      // create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      };
+      // Update log
+      this.logService.updateLog(updLog);
+    }
+
+    // Clear state
+    this.clearState();
+  }
+
+  clearState() {
+    this.isNew = true;
+    this.id = '';
+    this.text = '';
+    this.date = '';
+    this.logService.clearState();
+  }
+
+  generateId() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+          let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+          return v.toString(16);
+        });
+      }
+
+
+
+
+}
